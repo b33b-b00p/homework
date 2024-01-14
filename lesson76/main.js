c26b59addfae6722eff4eda9ca53b505a166e394
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-01-14 05:19:34 +0400

    added lesson 76

---
+++ lesson76/main.js
@@ -0,0 +1,10 @@
+const firstName = 'Petya';
+const lastName = 'Petrov';
+let age = 20;
+let favoriteColor = 'black';
+let favoriteNumber = '10';
+
+console.log(firstName, lastName);
+console.log(age + ' y.o.');
+console.log('favorite color: ' + favoriteColor);
+console.log('favorite number: ' + favoriteNumber);

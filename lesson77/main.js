4b1dbf85a9c00f168a2463ee4fc7dbf06028b192
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-01-14 06:32:49 +0400

    added lesson 77

---
+++ lesson77/main.js
@@ -0,0 +1,15 @@
+const x = 5;
+const y = 2;
+
+const sum = x + y;
+const subst = x - y;
+const multi = x * y;
+const div = x / y;
+
+console.log(sum);
+console.log(subst);
+console.log(multi);
+console.log(div);
+
+console.log(x % y);
+console.log(x ** y);

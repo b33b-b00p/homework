b7c4e414c6371afbf5ec594517ed24e8676ac83b
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-01-14 20:29:05 +0400

    added lesson 85

---
+++ lesson85/main.js
@@ -0,0 +1,8 @@
+const numbers = [1, 2, 3, 4, 5];
+console.log(numbers)
+numbers.pop();
+console.log(numbers + ' pop')
+numbers.shift();
+console.log(numbers + ' shift')
+numbers.unshift(9);
+console.log(numbers + ' unshift(9)');

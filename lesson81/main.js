a9164d60af3aca2d3e16e142894282473fdf0bcb
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-01-14 07:34:45 +0400

    added lesson 81

---
+++ lesson81/main.js
@@ -0,0 +1,13 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for(let i = 0; i < numbers.length; i++)
+{
+    if(numbers[i]%2===0)
+    {
+        console.log(numbers[i]);
+    }
+}
+const rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'purple'];
+for(let i = rainbow.length; i >= 0; i--)
+{
+    console.log(rainbow[i]);
+}

b8ad8e1ce818f79f1d437b1953ad808c69537f07
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-02-13 00:30:57 +0400

    added animated-text section

--- diploma/js/main.js
+++ diploma/js/main.js
@@ -19,4 +19,26 @@
             document.body.classList.remove('body--opened-menu');
         }
     }
+    //animated-text
+    const scrollers = document.querySelectorAll(".animated-text__scroller");
+
+    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) 
+    {
+        addAnimation();
+    }
+
+    function addAnimation() {
+        scrollers.forEach((scroller) => {
+            scroller.setAttribute("data-animated", true);
+            
+            const scrollerInner = scroller.querySelector('.animated-text__scroller-inner');
+            const scrollerContent = Array.from(scrollerInner.children);
+
+            scrollerContent.forEach((item) => {
+                const duplicatedItem = item.cloneNode(true);
+                duplicatedItem.setAttribute('aria-hidden', true);
+                scrollerInner.appendChild(duplicatedItem);
+            })
+        });
+    }
 })();

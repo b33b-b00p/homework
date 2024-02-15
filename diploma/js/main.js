00d650e64e13442b4b328d137971b1d3cbcac805
b33b-b00p <103651421+b33b-b00p@users.noreply.github.com>
2024-02-16 02:31:36 +0400

    added testimonials (no adaptive)

--- diploma/js/main.js
+++ diploma/js/main.js
@@ -41,4 +41,74 @@
             })
         });
     }
+    //testimonials
+    new Swiper('.testimonials__img-slider', {
+        slidesPerView: 3,
+        centeredSlides: true,
+        spaceBetween: 30,
+        lazyLoading: true,
+        initialSlide: 1,
+        loop: true,
+        speed: 800,
+
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+        
+        // breakpoints: {
+        //     901: {
+        //         slidesPerView: 1.5,
+        //     },
+        //     1201: {
+        //         slidesPerView: 2.1,
+        //     },
+        // },
+    });
+
+    new Swiper('.testimonials__author-slider', {
+        spaceBetween: 0,
+        slidesPerView: 1,
+        initialSlide: 1,
+        loop: true,
+        speed: 700,
+        // centeredSlides: true,
+
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+        
+        // breakpoints: {
+        //     901: {
+        //         slidesPerView: 1.5,
+        //     },
+        //     1201: {
+        //         slidesPerView: 2.1,
+        //     },
+        // },
+    });
+
+    new Swiper('.testimonials__quote-slider', {
+        spaceBetween: 0,
+        slidesPerView: 1,
+        initialSlide: 1,
+        loop: true,
+        speed: 700,
+        // centeredSlides: true,
+
+        navigation: {
+            nextEl: '.testimonials__next',
+            prevEl: '.testimonials__prev',
+        },
+        
+        // breakpoints: {
+        //     901: {
+        //         slidesPerView: 1.5,
+        //     },
+        //     1201: {
+        //         slidesPerView: 2.1,
+        //     },
+        // },
+    });
 })();
